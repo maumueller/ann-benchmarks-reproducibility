4161acf4ae073fe30b1a2fe5a2cb559847b0a274
Martin Aumüller <maau@itu.dk>
2020-08-31 10:33:10 +0200

    Updated experiment calls for python2/python3

--- reproducibility/run_experiments.sh
+++ reproducibility/run_experiments.sh
@@ -3,24 +3,28 @@ python3 install.py
 ALGODEF=reproducibility/standard_runs.yaml
 
 for ds in glove-100-angular sift-128-euclidean random-10nn-euclidean gist-960-euclidean nytimes-256-angular; do
-    python3 run.py --dataset $ds --definition $ALGODEF --runs 2 --parallelism 4 --count 10
+    python3 run.py --dataset $ds --definition $ALGODEF --runs 1 --count 10 --local
+    python run.py --dataset $ds --definition $ALGODEF --runs 1 --count 10 --local
 done
 
 
 # K=100 runs
 for ds in glove-100-angular sift-128-euclidean gist-960-euclidean; do
-    python3 run.py --dataset $ds --definition $ALGODEF --runs 2 --parallelism 4 --count 100
+    python3 run.py --dataset $ds --definition $ALGODEF --runs 1 --count 100 --local
+    python run.py --dataset $ds --definition $ALGODEF --runs 1 --count 100 --local
 done
 
 # Hamming
 ALGODEF=reproducibility/hamming_runs.yaml
 for ds in sift-256-hamming word2bits-800-hamming; do
-    python3 run.py --dataset $ds --definition $ALGODEF --runs 2 --parallelism 4 --count 10
+    python3 run.py --dataset $ds --definition $ALGODEF --runs 1 --count 10 --local
+    python run.py --dataset $ds --definition $ALGODEF --runs 1 --count 10 --local
 done
 
 # GPU
 ALGODEF=reproducibility/batch_runs.yaml
-python3 run.py --dataset sift-128-euclidean --definition $ALGODEF --batch --count 10
+python3 run.py --dataset sift-128-euclidean --definition $ALGODEF --batch --count 10 --local
+python run.py --dataset $ds --definition $ALGODEF --runs 1 --count 10 --local
 
 
 
