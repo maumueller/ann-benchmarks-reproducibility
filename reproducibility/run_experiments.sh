1eaf52b48307b8eff1f0a0f0aff589c31ffe3e40
Martin Aumueller <maau@itu.dk>
2021-01-26 20:37:57 +0100

    Use original docker files.

--- reproducibility/run_experiments.sh
+++ reproducibility/run_experiments.sh
@@ -1,33 +1,30 @@
-for ds in glove-100-angular sift-128-euclidean random-10nn-euclidean gist-960-euclidean nytimes-256-angular sift-256-hamming word2bits-800-hamming; do
-    python3 create_dataset.py --dataset $ds
-done
+#for ds in glove-100-angular sift-128-euclidean random-10nn-euclidean gist-960-euclidean nytimes-256-angular sift-256-hamming word2bits-800-hamming; do
+#    python3 create_dataset.py --dataset $ds
+#done
 
 # K=10 runs
 ALGODEF=reproducibility/standard_runs.yaml
 
 for ds in glove-100-angular sift-128-euclidean random-10nn-euclidean gist-960-euclidean nytimes-256-angular; do
-    python3 run.py --dataset $ds --definition $ALGODEF --runs 1 --count 10 --local
-    python run.py --dataset $ds --definition $ALGODEF --runs 1 --count 10 --local
+    python3 run.py --dataset $ds --definition $ALGODEF --runs 1 --count 10
 done
 
 
 # K=100 runs
 for ds in glove-100-angular sift-128-euclidean gist-960-euclidean; do
-    python3 run.py --dataset $ds --definition $ALGODEF --runs 1 --count 100 --local
-    python run.py --dataset $ds --definition $ALGODEF --runs 1 --count 100 --local
+    python3 run.py --dataset $ds --definition $ALGODEF --runs 1 --count 100
 done
 
 # Hamming
 ALGODEF=reproducibility/hamming_runs.yaml
 for ds in sift-256-hamming word2bits-800-hamming; do
-    python3 run.py --dataset $ds --definition $ALGODEF --runs 1 --count 10 --local
-    python run.py --dataset $ds --definition $ALGODEF --runs 1 --count 10 --local
+    python3 run.py --dataset $ds --definition $ALGODEF --runs 1 --count 10
 done
 
 # GPU
-ALGODEF=reproducibility/batch_runs.yaml
-python3 run.py --dataset sift-128-euclidean --definition $ALGODEF --batch --count 10 --local
-python run.py --dataset $ds --definition $ALGODEF --runs 1 --count 10 --local
+#ALGODEF=reproducibility/batch_runs.yaml
+#python3 run.py --dataset sift-128-euclidean --definition $ALGODEF --batch --count 10 --local
+#python run.py --dataset $ds --definition $ALGODEF --runs 1 --count 10 --local
 
 
 
