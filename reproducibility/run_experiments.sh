8ac850b4c53ac16936719a92cf48951fd78f5d64
Martin Aumueller <maau@itu.dk>
2021-02-05 12:23:18 +0100

    keep logs

--- reproducibility/run_experiments.sh
+++ reproducibility/run_experiments.sh
@@ -7,20 +7,25 @@ python=/home/maau/miniconda3.6/bin/python
 # K=10 runs
 ALGODEF=reproducibility/standard_runs.yaml
 
+mkdir -p logs
+
 for ds in glove-100-angular sift-128-euclidean random-10nn-euclidean gist-960-euclidean nytimes-256-angular; do
     $python run.py --dataset $ds --definition $ALGODEF --runs 3 --count 10 --parallelism 20
+    cp annb.log logs/${ds}_10.log
 done
 
 
 # K=100 runs
 for ds in glove-100-angular sift-128-euclidean gist-960-euclidean; do
     $python run.py --dataset $ds --definition $ALGODEF --runs 1 --count 100 --parallelism 20
+    cp annb.log logs/${ds}_100.log
 done
 
 # Hamming
 ALGODEF=reproducibility/hamming_runs.yaml
 for ds in sift-256-hamming word2bits-800-hamming; do
     $python run.py --dataset $ds --definition $ALGODEF --runs 1 --count 10 --parallelism 20
+    cp annb.log logs/${ds}_10.log
 done
 
 # GPU
